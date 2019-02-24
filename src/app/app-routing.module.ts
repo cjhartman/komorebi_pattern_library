92f873cee5b74d23a8084f8fa19b82d1834a87c8
CJ Hartman <cjhartmanx@gmail.com>
2019-02-24 17:30:29 -0500

    Initialization of pattern library - wired up global styles and bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BaseComponent } from './base/base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BaseComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { HeaderComponent } from './base/header/header.component';
+import { SidenavComponent } from './base/sidenav/sidenav.component';
+import { ContentComponent } from './base/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    ContentComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
