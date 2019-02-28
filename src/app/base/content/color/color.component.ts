54a0233cd44fdc01772349bee91e5615ff425f59
CJ Hartman <cjhartmanx@gmail.com>
2019-02-27 20:12:44 -0500

    Updated Colors page -- added a new feeling

--- src/app/base/content/color/color.component.ts
+++ src/app/base/content/color/color.component.ts
@@ -7,9 +7,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorComponent implements OnInit {
 
+  colors: any[] = [
+    {
+      color: 'base',
+      contentName: 'Fall',
+      contentValue: '#963A59'
+    },
+    {
+      color: 'secondary',
+      contentName: 'Sky',
+      contentValue: '#03B5AA'
+    },
+    {
+      color: 'blue',
+      contentName: 'Shadow Blue',
+      contentValue: '#0C1B33'
+    },
+    {
+      color: 'green',
+      contentName: 'Sunsoaked Green',
+      contentValue: '#DBFE87'
+    },
+    {
+      color: 'brown',
+      contentName: 'Shimmering Gold',
+      contentValue: '#C2BA51'
+    },
+    {
+      color: 'offWhite',
+      contentName: 'Bright',
+      contentValue: '#F0F0F0'
+    },
+    {
+      color: 'selectedOffWhite',
+      contentName: 'Silver Light',
+      contentValue: '#DCDFDF'
+    },
+    {
+      color: 'dark',
+      contentName: 'Night',
+      contentValue: '#161616'
+    }
+  ];
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  colorSwatch(colors: any) {
+    switch (colors) {
+      case 'base':
+        return '#963A59';
+      case 'secondary':
+        return '#03B5AA';
+      case 'blue':
+        return '#0C1B33';
+      case 'green':
+        return '#DBFE87';
+      case 'brown':
+        return '#C2BA51';
+      case 'offWhite':
+        return '#F0F0F0';
+      case 'selectedOffWhite':
+        return '#DCDFDF';
+      case 'dark':
+        return '#161616';
+    }
   }
 
 }
